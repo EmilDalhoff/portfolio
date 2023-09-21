8d6059d2fa2dd9eb0bb2edf34ae041d0832fd50a
EmilDalhoff <emildalhoff@hotmail.com>
2023-09-21 16:46:54 +0200

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,43 @@
-import { NavLink } from "react-router-dom";
+
 
 export default function Nav() {
-    return (
+    const openHamburger = () => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+        const navLink = document.querySelectorAll(".nav-menu li");
+        const bar = document.querySelectorAll(".bar");
+
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        navLink.forEach((link) => {
+            link.classList.toggle("active");
         
+        });
+        bar.forEach((bar) => {
+            bar.classList.toggle("active");
+        });
+    }
+
+    return (
+
         <nav>
             <img className="imgemil" src="src/assets/groenport.png" alt="nav-pic" />
 
-            
-             <div className="nav-bar" >
-            <a href="#intro-link">HJEM</a>
-            <a href="#aboutme-link">OM MIG</a>
-            <a href="#port-link">PORTFOLIO</a> 
-            <a href="#skills-link">KOMPETENCER</a>
-            <a href="#contact-link">KONTAKT</a>
+
+            <div className="nav-bar" >
+                <ul className="nav-menu">
+
+                    <li><a href="#intro-link">HJEM</a></li>
+                    <li><a href="#aboutme-link">OM MIG</a></li>
+                    <li><a href="#port-link">PORTFOLIO</a></li>
+                    <li><a href="#skills-link">KOMPETENCER</a></li>
+                    <li><a href="#contact-link">KONTAKT</a></li>
+                </ul>
+                <div className="hamburger" onClick={openHamburger}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             </div>
 
         </nav>
